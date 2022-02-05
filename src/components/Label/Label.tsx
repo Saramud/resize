5a81bb0d608012bac5cd805b02dbc6409d4c00ab
Sergey Saramud <sergey.saramud@taskdata.ru>
2022-02-05 20:45:29 +0300

    Добавил валидацию

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,31 +1,44 @@
 import * as React from 'react';
 import './Label.scss';
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt, faSave, faCircle,  } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Coordinate } from '../Preview/Preview';
 
 interface LabelProps {
-    xPosition: number,
-    yPosition: number,
-    handleDoubleClick: (index: number) => void;
+    labelInfo: Coordinate;
+    handleTrashClick: (index: number) => void;
+    handleSaveClick: (index: number) => void;
     index: number;
-    value: string;
     onChangeValue: (value: string, index: number) => void;
 }
 
 export const Label = (props: LabelProps) => {
-    const { xPosition, yPosition, handleDoubleClick, onChangeValue, index, value } = props;
-
+    const { labelInfo, handleTrashClick, handleSaveClick, onChangeValue, index } = props;
     return (
-        <div className='label' style={{ top: `${yPosition}%`, left: `${xPosition}%` }}>
-            <FontAwesomeIcon 
-                className="label_fa"
-                color={'#1ab335'}
-                icon={faTrashAlt}
-                onClick={() => handleDoubleClick(index)} />
-            <input type='text' value={value}    
-                onChange={
-                    (e: React.ChangeEvent<HTMLInputElement>) => onChangeValue(e.target.value, index)
-                } />
+        <div className={labelInfo.hint ? 'label labelValidation' : 'label'} style={{ top: `${labelInfo.yPosition}%`, left: `${labelInfo.xPosition}%` }}>
+            <div className='inputWrapper'>
+            <FontAwesomeIcon
+                        color={'#1ab335'}
+                        icon={faCircle}
+                        style={{transform:'rotate(150deg)'}}/>
+                {/* <input type='text' value={labelInfo.value}
+                    onChange={
+                        (e: React.ChangeEvent<HTMLInputElement>) => onChangeValue(e.target.value, index)
+                    } />
+                {labelInfo.isSave ?
+                    <FontAwesomeIcon
+                        className="label_fa"
+                        color={'#1ab335'}
+                        icon={faTrashAlt}
+                        onClick={() => handleTrashClick(index)} /> :
+
+                    <FontAwesomeIcon
+                        className="label_fa"
+                        color={'#1ab335'}
+                        icon={faSave}
+                        onClick={() => handleSaveClick(index)} />} */}
+            </div>
+            {labelInfo.hint && <p className='hint'>{labelInfo.hint}</p>}
         </div>
     )
 } 
--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -8,17 +8,19 @@ interface ViewPortProps {
     event: React.ChangeEvent<HTMLInputElement> | null;
 }
 
-interface Coordinate {
+export interface Coordinate {
     xPosition: number,
     yPosition: number,
     value: string,
+    hint: string | null,
+    isSave: boolean,
 }
 
 export const Preview = (props: ViewPortProps) => {
     const { event } = props;
     const [coordinates, setCoordinate] = React.useState<Coordinate[] | null>(null);
     const targetRef = React.useRef<HTMLHeadingElement>(null);
-    
+
     let file = null;
 
     React.useEffect(() => {
@@ -26,20 +28,44 @@ export const Preview = (props: ViewPortProps) => {
     }, [event])
 
     const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        const updateCoordinates = coordinates && [...coordinates];
+        const length = updateCoordinates?.length;
+        if (length && updateCoordinates[length - 1].isSave === false) {
+            return setCoordinate(list => {
+                if (list?.length) {
+                    list[length - 1].hint = 'Сохраните метку!';
+                    return [...list]
+                }
+                return list;
+            });
+        }
         const newCoordinates = {
             xPosition: getPercent(targetRef.current?.offsetWidth, e.nativeEvent.offsetX),
             yPosition: getPercent(targetRef.current?.offsetHeight, e.nativeEvent.offsetY),
             value: '',
+            hint: null,
+            isSave: false,
         };
+
         setCoordinate(list => {
             return list?.length ? [...list, newCoordinates] : [newCoordinates]
         })
     }
 
-
-    const handleDoubleClick = (index: number) => {
+    const handleTrashClick = (index: number) => {
         setCoordinate(list => {
             return list?.length ? [...list.filter((el, id) => id !== index)] : list;
+        });
+    }
+
+    const handleSaveClick = (index: number) => {
+        setCoordinate(list => {
+            if (list?.length) {
+                list[index].isSave = true;
+                list[index].hint = null;
+                return [...list]
+            }
+            return list;
         });
     }
 
@@ -62,12 +88,12 @@ export const Preview = (props: ViewPortProps) => {
             }
             {file && coordinates && coordinates.map((coordinate, index) => {
                 return (
-                    <Label xPosition={coordinate.xPosition}
-                        yPosition={coordinate.yPosition}
-                        value={coordinate.value}
+                    <Label
+                        labelInfo={coordinate}
                         key={index}
                         index={index}
-                        handleDoubleClick={handleDoubleClick}
+                        handleTrashClick={handleTrashClick}
+                        handleSaveClick={handleSaveClick}
                         onChangeValue={onChangeValue} />
                 )
             })
